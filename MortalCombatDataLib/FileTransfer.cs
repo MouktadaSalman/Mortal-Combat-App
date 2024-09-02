48e786a470211f43403a7a69adcb68ce85dd5e9e
MJQ <21494299@student.curtin.edu.au>
2024-09-02 15:47:57 +0800

    Create file transfer module

---
+++ MortalCombatDataLib/FileTransfer.cs
@@ -0,0 +1,20 @@
+﻿/* 
+ * Module: FileTransfer
+ * Description: The operations to copy files from client
+ *              to server and vice-versa
+ * Author: Jauhar
+ * ID: 21494299
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MortalCombatDataLib
+{
+    public class FileTransfer
+    {
+    }
+}
