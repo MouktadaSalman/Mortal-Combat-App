5765074c7fce2d7c60fe3a596336b66fc088e815
Ahmedo-o <a8y08888@gmail.com>
2024-09-06 14:15:05 +0800

    This should work for lobby.

--- MortalCombatBusinessServer/BusinessInterfaceImpl.cs
+++ MortalCombatBusinessServer/BusinessInterfaceImpl.cs
@@ -197,6 +197,7 @@ namespace MortalCombatBusinessServer
         }
 
 
+
         //lobby messages
         public void DistributeMessageToLobby(string lobbyName, string sender, object content)
         {
